da69e1dd50e549626efbb1884ae040f517146643
lilyevans <evanslm2@g.cofc.edu>
2018-09-27 20:55:48 -0500

    Initial Commit

---
+++ BusArrivalPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BusArrivalPractice
+//
+//  Created by lindsay evans on 9/27/18.
+//  Copyright © 2018 lindsay.evans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
